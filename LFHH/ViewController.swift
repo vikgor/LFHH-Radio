3ddf2b73d08c4dcca6d06a28b3aca870ff908caf
Viktor <viktorgo22@gmail.com>
2019-06-30 19:37:07 +0300

    Initial Commit

---
+++ LFHH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LFHH
+//
+//  Created by Viktor Gordienko on 6/30/19.
+//  Copyright © 2019 Viktor Gordienko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

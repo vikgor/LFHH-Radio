45bef1a242d7a1e651a1615c44d6fd00d3bd2150
Viktor <viktorgo22@gmail.com>
2019-06-30 21:46:28 +0300

    Update

--- LFHH/ViewController.swift
+++ LFHH/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var radioButton: UIButton!
+    var isPlaying = false
+    var radioPlayer = AVPlayer()
+    var playerItem: AVPlayerItem!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        radioButton.setTitle("Play", for: .normal)
+        
+//        let playBackURL = "https://nashe1.hostingradio.ru:80/nashe-64.mp3"
+        let playBackURL = URL(string: "http://62.109.25.83:8000/lofi")
+        playerItem = AVPlayerItem(url: playBackURL!)
+        radioPlayer = AVPlayer(playerItem: playerItem)
+        playerItem.addObserver(self, forKeyPath: "timedMetadata", options: NSKeyValueObservingOptions(), context: nil)
+//        radioPlayer.play()
 
+
+    }
+    
+    override func observeValue(forKeyPath: String?, of: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if forKeyPath != "timedMetadata" { return }
+        let data: AVPlayerItem = of as! AVPlayerItem
+        for item in data.timedMetadata! {
+            print(item.value!)
+        }
+    }
+    
+    @IBAction func playRadio(_ sender: UIButton) {
+        radioPlayer.play()
+        
+        switch isPlaying {
+        case false:
+            radioPlayer.play()
+            isPlaying.toggle()
+            print(isPlaying)
+            radioButton.setTitle("Stop", for: .normal)
+            
+        default:
+            radioPlayer.pause()
+            isPlaying.toggle()
+            print(isPlaying)
+            radioButton.setTitle("Play", for: .normal)
+        }
+    }
+    
+    
 
 }
 
